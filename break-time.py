67d6bf4fd6baeb370effbc9d4bda661668147a60
Shumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2017-05-16 22:13:26 -0400

    Initial commit.
    Adds a simple rickroll loop that continues until interrupted by user. Waits ten seconds before opening browser with rickroll.

---
+++ break-time.py
@@ -0,0 +1,7 @@
+import time
+import webbrowser
+
+print("This program was started on "+time.ctime())
+while True:
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
